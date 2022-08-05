540207601d9172913ccff49b152f2f6454a2a60b
cnboker <coobots@gamil.com>
2022-08-05 14:49:04 +0800

    update

--- pack/docusaurus.config.js
+++ pack/docusaurus.config.js
@@ -95,7 +95,9 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} ioliz.com, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} ioliz.com <br/> 
+        <a target="_blank" href="http://www.miibeian.gov.cn/" class="scrollto">粤ICP备18034927号-1</a> &nbsp;
+        <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44030702004919"><img src="http://www.beian.gov.cn/img/ghs.png">粤公网安备 44030702004919号</a>`,
       },
       prism: {
         theme: lightCodeTheme,
--- pack/src/pages/index.js
+++ pack/src/pages/index.js
@@ -5,6 +5,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Company from '@site/src/components/CompanyFeatures/company.js'
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -29,11 +30,12 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="数字标牌应用解决方案">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
+        <Company />
       </main>
     </Layout>
   );
